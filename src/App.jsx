03f5e9ed96c6babba08be2e5e01adb4d66c2d7a0
George Jones <georgekeesjones@gmail.com>
2025-08-13 11:07:27 +0300

    add pets to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [pets, setPets] = useState([])
 
   const getAllPets = async () => {
     // const url = 'http://localhost:3000/pets/'
@@ -9,6 +11,7 @@ const App = () => {
     const url = `${import.meta.env.VITE_BACKEND_URL}/pets`
     const response = await axios.get(url)
     console.log(response)
+    setPets(response.data)
   }
 
   useEffect(() => {
@@ -18,6 +21,17 @@ const App = () => {
   return (
     <>
       <h1>Welcome to the pets app!</h1>
+      <ol>
+        {
+          pets.length
+            ?
+            pets.map(pet => {
+              return <p>{pet.name}</p>
+            })
+            :
+            <p>Loading Pets</p>
+        }
+      </ol>
     </>
   )
 }
